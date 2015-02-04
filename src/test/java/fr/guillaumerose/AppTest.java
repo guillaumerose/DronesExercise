c7c6229bc2d342a8a5ce0813099e6aba4abe7575
Guillaume Rose <guillaume.rose@mappy.com>
2015-02-04 17:49:50 +0100

    Second acceptance test is ok

--- src/test/java/fr/guillaumerose/AppTest.java
+++ src/test/java/fr/guillaumerose/AppTest.java
@@ -7,8 +7,13 @@ import static org.assertj.core.api.Assertions.*;
 
 public class AppTest {
     @Test
-    public void should_answer_acceptance_test() {
+    public void should_answer_to_first_acceptance_test() {
         // assertThat(readLine("DRONE_A;7 10 N;AAGAAADAAGAAAGADAA")).isEqualTo("DRONE_A : 13 0 O");
+    }
+
+    @Test
+    public void should_answer_to_second_acceptance_test() {
+        assertThat(readLine("DRONE_B;13 23 E;AAADAAGAAADAAGAAAAAADA")).isEqualTo("DRONE_B : 25 18 S");
     }
 
     @Test
@@ -43,5 +48,11 @@ public class AppTest {
     public void should_be_able_to_turn_right() throws Exception {
         assertThat(readLine("DRONE_C;0 0 N;D")).isEqualTo("DRONE_C : 0 0 E");
         assertThat(readLine("DRONE_C;10 5 S;D")).isEqualTo("DRONE_C : 10 5 O");
+    }
+
+    @Test
+    public void should_be_stable() throws Exception {
+        assertThat(readLine("DRONE_C;0 0 N;AAGAAGAAGAAG")).isEqualTo("DRONE_C : 0 0 N");
+        assertThat(readLine("DRONE_C;0 0 N;AAADAAADAAADAAAD")).isEqualTo("DRONE_C : 0 0 N");
     }
 }
